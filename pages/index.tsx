ad230fdfa4bedb6a252ddf905b1bb665fd5e4f7d
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-06-26 08:17:55 +0800

    update product listing image dimension

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,13 @@ export default function Home() {
             <div
               className="flex flex-col h-full bg-white relative pb-14 pl-7 pr-7 pt-7 md:pl-4 md:pt-4 md:pr-4"
               key={product.id}>
-              <img src={product.images[0]} alt={product.title} />
+              <div className="flex items-center justify-center w-full h-[150px]">
+                <img
+                  src={product.images[0]}
+                  alt={product.title}
+                  className="object-cover h-full w-full"
+                />
+              </div>
               <h3 className="text-md mt-5 font-semibold mb-4">
                 {product.title}
               </h3>
