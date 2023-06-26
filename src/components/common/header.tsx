fcb4c00c405b8873092f8f2a6436f9a94e65107d
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-06-26 09:25:31 +0800

    add base link for store header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const Header: React.FC<{}> = () => {
   return (
     <header className="bg-gray-900 text-white py-4 mb-10">
       <div className="container mx-auto flex items-center justify-between px-4">
-        <h1 className="text-2xl font-bold">Web store</h1>
+        <h1 className="text-2xl font-bold">
+          <Link href="/">Web store</Link>
+        </h1>
         <nav className="flex items-center">
           <div className="flex items-center mr-4">
             <img src="../assets/cart.svg" alt="Cart" />
